3c6aa5a1fa774049e0f15b7ac0530d73a55a1477
Ethan Clatterbaugh <ethan.sea17@gmail.com>
2022-08-27 02:34:42 -0600

    fixing connection strings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 
 use zero2prod::configuration::get_configuration;
@@ -7,7 +8,10 @@ use zero2prod::startup::run;
 async fn main() -> std::io::Result<()> {
     // Panic if we can't get configuration
     let configuration = get_configuration().expect("Failed to read configuration");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
-    let listener = TcpListener::bind(address);
-    run(listener?)?.await
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection)?.await
 }
