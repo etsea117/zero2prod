bcece08f42a8e351fef5c7bff879aee46d82651d
Ethan Clatterbaugh <ethan.sea17@gmail.com>
2022-10-13 20:35:02 -0600

    shift responsibility boundaries for email validation 9.8.2

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -17,15 +17,32 @@ pub async fn publish_newsletter(
 ) -> Result<HttpResponse, PublishError> {
     let subscribers = get_confirmed_subscribers(&pool).await?;
     for subscriber in subscribers {
-        email_client
-            .send_email(
-                subscriber.email,
-                &body.title,
-                &body.content.html,
-                &body.content.text,
-            )
-            .await
-            .with_context(|| format!("Failed to send newsletter issue to {}", subscriber.email))?;
+        match subscriber {
+            Ok(subscriber) => {
+                email_client
+                    .send_email(
+                        &subscriber.email,
+                        &body.title,
+                        &body.content.html,
+                        &body.content.text,
+                    )
+                    .await
+                    .with_context(|| {
+                        format!("Failed to send newsletter issue to {}", subscriber.email)
+                    })?;
+            }
+            Err(error) => {
+                tracing::warn!(
+                    // We record the error chain as a structured field
+                    // on the log record.
+                    error.cause_chain = ?error,
+                    // Using `\` to split a long string literal over
+                    // two lines, without creating a `\n` character.
+                    "Skipping a confirmed subscriber. \
+                    Their stored contact details are invalid",
+                );
+            }
+        }
     }
     Ok(HttpResponse::Ok().finish())
 }
@@ -49,11 +66,12 @@ struct ConfirmedSubscriber {
 #[tracing::instrument(name = "Get confirmed subscribers", skip(pool))]
 async fn get_confirmed_subscribers(
     pool: &PgPool,
-) -> Result<Vec<ConfirmedSubscriber>, anyhow::Error> {
-    // We only need `Row` to map the data coming out of this query.
-    // Nesting its definition inside the function itself is a simple way
-    // to clearly communicate this coupling (and to ensure it doesn't
-    // get used elsewhere by mistake).
+    // We are returning a `Vec` of `Result`s in the happy case.
+    // This allows the caller to bubble up errors due to network issues or other
+    // transient failures using the `?` operator, while the compiler
+    // forces them to handle the subtler mapping error.
+    // See http://sled.rs/errors.html for a deep-dive about this technique.
+) -> Result<Vec<Result<ConfirmedSubscriber, anyhow::Error>>, anyhow::Error> {
     struct Row {
         email: String,
     }
@@ -68,18 +86,13 @@ async fn get_confirmed_subscribers(
     )
     .fetch_all(pool)
     .await?;
-    // Map into the domain type
+
     let confirmed_subscribers = rows
         .into_iter()
-        .filter_map(|r| match SubscriberEmail::parse(r.email) {
-            Ok(email) => Some(ConfirmedSubscriber { email }),
-            Err(error) => {
-                tracing::warn!(
-                    "A confirmed subscriber is using an invalid email address.\n{}.",
-                    error
-                );
-                None
-            }
+        // No longer using `filter_map`!
+        .map(|r| match SubscriberEmail::parse(r.email) {
+            Ok(email) => Ok(ConfirmedSubscriber { email }),
+            Err(error) => Err(anyhow::anyhow!(error)),
         })
         .collect();
     Ok(confirmed_subscribers)
