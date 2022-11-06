0cc99ef76cb83acad51c3da10b15285bde3baa70
Ethan Clatterbaugh <ethan.sea17@gmail.com>
2022-11-06 12:44:49 -0700

    add html pages for home and login 10.6

--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,8 @@
 use crate::configuration::DatabaseSettings;
 use crate::configuration::Settings;
 use crate::email_client::EmailClient;
-use crate::routes::{confirm, health_check, publish_newsletter, subscribe};
+use crate::routes::home;
+use crate::routes::{confirm, health_check, login_form, publish_newsletter, subscribe};
 use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
@@ -78,6 +79,8 @@ pub fn run(
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
+            .route("/", web::get().to(home))
+            .route("/login", web::get().to(login_form))
             .route("/newsletters", web::post().to(publish_newsletter))
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
