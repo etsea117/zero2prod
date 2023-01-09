9339c5ac3e4f22f61737bc68b6cc4f16af8e9ab1
Ethan Clatterbaugh <ethan.sea17@gmail.com>
2023-01-09 14:55:26 -0700

    actix-web-flash-messages ch 10.6.4.15

--- src/startup.rs
+++ src/startup.rs
@@ -3,8 +3,12 @@ use crate::configuration::Settings;
 use crate::email_client::EmailClient;
 use crate::routes::home;
 use crate::routes::{confirm, health_check, login, login_form, publish_newsletter, subscribe};
+use actix_web::cookie::Key;
 use actix_web::web::Data;
 use actix_web::{dev::Server, web, App, HttpServer};
+use actix_web_flash_messages::storage::CookieMessageStore;
+use actix_web_flash_messages::FlashMessagesFramework;
+use secrecy::ExposeSecret;
 use secrecy::Secret;
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
@@ -80,8 +84,12 @@ fn run(
     let db_pool = web::Data::new(db_pool);
     let email_client = web::Data::new(email_client);
     let base_url = web::Data::new(ApplicationBaseUrl(base_url));
+    let message_store =
+        CookieMessageStore::builder(Key::from(hmac_secret.0.expose_secret().as_bytes())).build();
+    let message_framework = FlashMessagesFramework::builder(message_store).build();
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(message_framework.clone())
             .wrap(TracingLogger::default())
             .route("/", web::get().to(home))
             .route("/login", web::get().to(login_form))
